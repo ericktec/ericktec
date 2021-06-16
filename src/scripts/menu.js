73a548174de3d77cb43f8efedd110dc85797864b
Erick <erick_tec@outlook.com>
2021-06-16 11:45:26 -0500

    fix: pop up scroll

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,27 +1,31 @@
 const navigationMenu = () => {
   const menu = document.getElementById("navigation__checkbox");
   const navigationBackground = document.getElementById("navigation__background");
-
-  window.addEventListener("keypress", (e) => {
-    if(e.key === "Escape"){
-      console.log("se presiono escape")
-    }
-    console.log(e);
-  });
+  let scrollPosition = 0;
 
   window.addEventListener("click", (e) => {
     console.log(e.target);
     if(e.target === navigationBackground) {
       menu.checked = false;
+      menu.dispatchEvent(new Event("change", {"bubbles": true}));
     }
   });
 
   menu.addEventListener("change", () => {
+    let body = document.getElementsByTagName("body")[0];
     if(menu.checked) {
-      console.log("menu checked");
+      scrollPosition = window.scrollY;
+      body.style.position = "fixed";
+      body.style.width = "100%";
+      document.body.style.top = `-${scrollPosition}px`;
+      document.body.style.left = `0`;
     }
     else {
-      console.log("menu no checked");
+      console.log(scrollPosition);
+      body.style.position = "";
+      body.style.overflow = "";
+      body.style.top = "";
+      window.scrollTo(0, scrollPosition );
     }
   });
 
